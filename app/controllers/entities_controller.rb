27078a881a1a79d47f59d6216ed6a1e420448f97
gersonhiggins <gahg.10gerson@gmail.com>
2023-10-19 13:39:12 -0400

    styles and linters

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -1,40 +1,40 @@
 class EntitiesController < ApplicationController
-    before_action :authenticate_user!
-    def show
-      @group = Group.find(params[:group_id])
-      @entity = Entity.find(params[:id])
-    end
-  
-    def new
-      @entity = Entity.new
-      @group = Group.find(params[:group_id])
-      @groups = Group.includes(:user).where(user: { id: current_user.id })
-    end
-  
-    def create
-      group_ids = params[:entity][:group_ids]
-  
-      if group_ids.nil?
-        flash[:alert] = 'Something went wrong'
-        redirect_to new_group_entity_path
-      else
-        group_ids.each do |group_id|
-          entitiy = current_user.entities.new(
-            name: params[:entity][:name],
-            amount: params[:entity][:amount],
-            group_id:
-          )
-          entitiy.save
-        end
-  
-        flash[:notice] = 'Transaction Created!'
-        redirect_to group_path(group_ids.first)
+  before_action :authenticate_user!
+  def show
+    @group = Group.find(params[:group_id])
+    @entity = Entity.find(params[:id])
+  end
+
+  def new
+    @entity = Entity.new
+    @group = Group.find(params[:group_id])
+    @groups = Group.includes(:user).where(user: { id: current_user.id })
+  end
+
+  def create
+    group_ids = params[:entity][:group_ids]
+
+    if group_ids.nil?
+      flash[:alert] = 'Something went wrong'
+      redirect_to new_group_entity_path
+    else
+      group_ids.each do |group_id|
+        entitiy = current_user.entities.new(
+          name: params[:entity][:name],
+          amount: params[:entity][:amount],
+          group_id:
+        )
+        entitiy.save
       end
-    end
-  
-    private
-  
-    def params_entity
-      params.require(:entity).permit(:author_id, :amount, :name, group_ids: [])
+
+      flash[:notice] = 'Transaction Created!'
+      redirect_to group_path(group_ids.first)
     end
   end
+
+  private
+
+  def params_entity
+    params.require(:entity).permit(:author_id, :amount, :name, group_ids: [])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  devise_scope :user do
+    delete 'logout', to: 'devise/sessions#destroy', as: :logout
+  end
+  
   root to: "users#index"
   
   resources :groups do
